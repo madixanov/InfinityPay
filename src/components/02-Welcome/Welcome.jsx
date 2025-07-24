6732e4510806f8f0b2f5781c6c05f0d2e57b02a0
Нщлги <yoqub.madixanov@outlook.com>
2025-07-24 19:56:27 +0500

    Adapting for devices

--- src/components/02-Welcome/Welcome.jsx
+++ src/components/02-Welcome/Welcome.jsx
@@ -10,20 +10,24 @@ export default function Welcome() {
   const { t } = useTranslation();
 
   return (
-    <div className="welcome-page" id="welcome">
-      <div className="container">
-        <div className="welcome-main">
-          <div className="text-container">
-            <h1 data-aos="fade-up" data-aos-duration="1000"><span>{t("page_one.welcome_span")}</span><br />{t("page_one.welcome")}</h1>
-            <h3 data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">{t("page_one.bridge")}</h3>
-            <p data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">{t("page_one.small_desc")}</p>
-            <button data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">{t("submit_text")}</button>
-          </div>
-          <div className="photo-container">
-            <img src={card} alt="Card Example" className="card-1"/>
-            <img src={card} alt="Card Example" className="card-2"/>
+    <div className="card-container">
+      <div className="welcome-page" id="welcome">
+        <div className="container">
+          <div className="welcome-main">
+            <div className="text-container">
+              <h1 data-aos="fade-up" data-aos-duration="1000"><span>{t("page_one.welcome_span")}</span><br />{t("page_one.welcome")}</h1>
+              <h3 data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">{t("page_one.bridge")}</h3>
+              <p data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">{t("page_one.small_desc")}</p>
+              <button data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">{t("submit_text")}</button>
+            </div>
+            <div className="photo-container">
+            </div>
           </div>
         </div>
+      </div>
+      <div className="cards-wrapper">
+        <img src={card} alt="Card Example" className="card-1"/>
+        <img src={card} alt="Card Example" className="card-2"/>
       </div>
     </div>
   )
